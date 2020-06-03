14dcd2b03bca550e42c6a6e236e5030876e2280b
BruceBAWest <brucebawest@gmail.com>
2020-06-03 10:00:13 -0700

    add links to menu

--- src/App.js
+++ src/App.js
@@ -2,36 +2,37 @@ import React from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main'
+import { Link } from 'react-router-dom';
 
 
 function App() {
-  return (
-    <div className="demo-big-content">
-    {/* creates menu layout from template, from React MDL framework */}
-    <Layout>
-        <Header title="Title" scroll>
-            <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-            </Navigation>
-        </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
-</div>
-  );
+    return (
+        <div className="demo-big-content">
+            {/* creates menu layout from template, from React MDL framework */}
+            <Layout>
+                <Header title="Title" scroll>
+                    <Navigation>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Header>
+                <Drawer title="Title">
+                    <Navigation>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <div className="page-content" />
+                    <Main/>
+                </Content>
+            </Layout>
+        </div>
+    );
 }
 
 export default App
