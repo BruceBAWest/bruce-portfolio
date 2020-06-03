fa9934d092e869d95a88a1c1b7ae3688d88a375b
BruceBAWest <brucebawest@gmail.com>
2020-06-03 13:31:26 -0700

    added some styling and avatar
    not a big change, but I need to save to step away.

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,7 +7,18 @@ class Landing extends Component {
         return(
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className="landing-grid">
-                    <Cell col={12}></Cell>
+                    <Cell col={12}>
+                        <img src="https://bruces-public-bucket.s3.amazonaws.com/brucebawest-avatar.jpg" 
+                        alt="avatar" 
+                        className="avatar-img" />
+                        <div className="banner-text">
+                            <h1>Bruce B. A. West</h1>
+                            <hr/>
+                            <h2>Software Development Engineer</h2>
+                            <hr/>
+                            <p>Java | JavaScript | Python | SQL | AWS | Linux | Mobile App Development | ReactJS | NodeJS</p>
+                        </div>
+                    </Cell>
                 </Grid>
             </div>
         )
