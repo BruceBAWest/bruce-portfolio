a358b6d4acd5e531aebdcbb85ee0a4d9c11f5f5f
BruceBAWest <brucebawest@gmail.com>
2020-06-04 07:57:32 -0700

    update before stepping away from this project temporarily for a work project

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -17,6 +17,20 @@ class Landing extends Component {
                             <h2>Software Development Engineer</h2>
                             <hr/>
                             <p>Java | JavaScript | Python | SQL | AWS | Linux | Mobile App Development | ReactJS | NodeJS</p>
+                        <div className="social-links">
+                            {/* LinkedIn */}
+                            <a href="www.linkedin.com/in/brucebawest" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                            </a>
+                            {/* GitHub */}
+                            <a href="https://github.com/BruceBAWest" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-github" aria-hidden="true"/>
+                            </a>
+                            {/* The J-O-B */}
+                            <a href="https://docs.aws.amazon.com/" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-briefcase" aria-hidden="true"/>
+                            </a>
+                        </div>
                         </div>
                     </Cell>
                 </Grid>
